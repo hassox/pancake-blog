76c793a04d9e2f1be2d451fd262f31e5ac6e4cff
Daniel Neighman <has.sox@gmail.com>
2009-11-21 09:25:32 +1100

    Changes back to optional routes.  Need pancake0.1.22

--- lib/blog/blog.rb
+++ lib/blog/blog.rb
@@ -1,11 +1,11 @@
 class Blog
 
-  get "/", :_name => :home do
+  get "(/)", :_name => :home do
     v.blog_entries = stack_class::BlogEntry.all
     render :root
   end
 
-  post "/" do
+  post "(/)" do
     blog_entry = stack_class::BlogEntry.new(params.blog_entry)
     blog_entry.save
     redirect url(:home)
